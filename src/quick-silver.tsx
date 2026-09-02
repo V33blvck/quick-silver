b9679bc11bcca260f681841e43aa89c476df416a
Refentse Pelotona <refentse50@gmail.com>
2026-09-02 21:16:33 +0200

    switched to Ids and added reorder controls

--- src/quick-silver.tsx
+++ src/quick-silver.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 
 function QuickSilver_toDo() {
   const [tasks, setTasks] = useState<Task[]>([]);
